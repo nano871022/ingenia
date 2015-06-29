59cb2addfe7ff90bcad1eaccaefade0ceebf7f0c
andresjpulido <andresjpulido@gmail.com>
2015-06-28 21:44:13 -0500

    proyecto eclipse modulo usuarios comunes

---
+++ adminUsuariosComunes/src/org/adminUsuarios/comunes/excepcion/AdaptadorException.java
@@ -0,0 +1,10 @@
+package org.adminUsuarios.comunes.excepcion;
+
+public class AdaptadorException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8405248572407537651L;
+
+}
